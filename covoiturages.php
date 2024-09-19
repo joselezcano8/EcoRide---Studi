d99352a1437cfa68e924765bd54dd61df75842ee
Jose Lezcano <joseleezcanog@gmail.com>
2024-09-19 09:22:00 +0200

    Covoiturage php

--- covoiturages.php
+++ covoiturages.php
@@ -8,6 +8,12 @@ $secondTitle = '';
 include 'inc/header.inc.php';
 ?>
 
-<main class="main">
-    <?php include 'inc/itineraire.inc.php'; ?>
-</main>
+        <main class="main">
+            <?php include 'inc/itineraire.inc.php'; ?>
+        </main>
+
+        <?php include 'inc/footer.inc.php'; ?>
+            <script src="script-itineraire.js"></script>
+            <script src="modal.js"></script>
+    </body>
+</html>
--- inc/header.inc.php
+++ inc/header.inc.php
@@ -35,7 +35,7 @@
         </ul>
     </nav>
     <!-- Titre, sous-titre et bg image -->
-    <header class="header" style="background-image: url('<?php echo $headerImg; ?>');width: 100ww; height: 450px; background-repeat: no-repeat">
+    <header class="header" style="background-image: url('<?php echo $headerImg; ?>'); height: 450px; background-repeat: no-repeat">
         <h1 style="color: var(<?php echo $titleColor; ?>)"><?php echo $firstTitle; ?></h1>
         <h3 style="color: var(<?php echo $titleColor; ?>)"><?php echo $secondTitle; ?>.</h3>
         <!--Overlay-->
